8d8bcca6bb8a13261d8a37423e6088b81d6a7771
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-15 16:29:43 -0400

    image link form added and styled

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+    return (
+        <div className="flex flex-column">
+            <p className="f3 center ">
+                {'Detect the faces in your pictures. Enjoy this app with any picture you want to play around with!'}
+            </p>
+            <div className="flex">
+                <div className="form center pa4 br3 shadow-5">
+                    <input type="text" className="f4 pa2 w-70 center" />
+                    <button className="w-30 center grow f4 link ph3 pv2 dib white bg-mid-gray ba b--mid-gray">Detect</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ImageLinkForm;
