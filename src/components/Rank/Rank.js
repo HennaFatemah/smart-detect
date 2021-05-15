a2fa19fff0c5a173d1f960ce7656a27eef37f7e4
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-15 16:35:50 -0400

    rank added

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div className="flex flex-column">
+            <div className="mid-gray f3 center">
+                {'Henna, your current rank is ...'}
+            </div>
+            <div className="mid-gray f1 center">
+                {'#5'}
+            </div>
+        </div>
+    );
+};
+
+export default Rank;
