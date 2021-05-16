4762bff9ff1a50ad50354d0b84498750396e50a7
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-16 19:54:50 -0400

    face recognition component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import './App.css';
@@ -64,7 +65,7 @@ class App extends Component {
           onImageUrlChange = {this.onImageUrlChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecognition/> */}
+        <FaceRecognition/>
       </div>
     );
   }
