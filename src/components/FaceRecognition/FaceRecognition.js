85e71e19eb1c0bc1bc2c51286a216f827f2fa007
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-17 10:29:47 -0400

    single face detection functionality and styling added

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div>
-            
+        <div className="absolute z 10 ma flex justify-center items-center w-100">
+            <div className="relative mt2">
+                <img id='inputimage'  className="relative z9 w-500 h-auto"  src={imageUrl} alt="" />
+                <div  className="absolute ba b--hot-pink shadow-1 z12 flex flex-wrap justify-center" style={{top: box.topRow, bottom: box.bottomRow, left: box.leftCol, right: box.rightCol}}></div>
+            </div>
         </div>
     );
 };
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onImageUrlChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div className="flex flex-column">
             <p className="f3 center ">
@@ -9,7 +9,7 @@ const ImageLinkForm = ({ onImageUrlChange, onButtonSubmit }) => {
             </p>
             <div className="flex">
                 <div className="form center pa4 br3 shadow-5">
-                    <input type="text" onChange={ onImageUrlChange } className="f4 pa2 w-70 center" />
+                    <input type="text" onChange={ onInputChange } className="f4 pa2 w-70 center" />
                     <button onClick={ onButtonSubmit } className="w-30 center grow f4 link ph3 pv2 dib white bg-mid-gray ba b--mid-gray">Detect</button>
                 </div>
             </div>
