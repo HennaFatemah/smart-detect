1e9cbd32d5a82bbdc1ebd6dac84f23e83ffd18a0
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-18 15:54:15 -0400

    width added tas inline attribute

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className="absolute z 10 ma flex justify-center items-center w-100">
             <div className="relative mt2">
-                <img id='inputimage'  className="relative z9 w-500 h-auto"  src={imageUrl} alt="" />
+                <img id='inputimage'  className="relative z9 h-auto" width='500px'  src={imageUrl} alt="" />
                 <div  className="bounding-box z12" style={{top: box.topRow, bottom: box.bottomRow, left: box.leftCol, right: box.rightCol}}></div>
             </div>
         </div>
