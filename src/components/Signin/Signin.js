86237dcf29cfadb88c304c67122be9e51d3a1cf7
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-19 08:03:55 -0400

    signin component changed to class component

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,50 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Signin = ({ onRouteChange }) => {
-    return (
-        <div className="relative z13 br3 ba pa3 dark-gray b--black-10 mv4 w-100 w-50-m mw6 shadow-5 w-25-l mw-5 flex justify-center items-center center">
-            <div className="measure tc">
-                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f3 fw6 ph0 mh0">Sign In</legend>
-                    <div className="mt3">
-                        <label 
-                            className="db fw6 lh-copy f6"
-                            htmlFor="email-address"
-                        >Email</label>
-                        <input 
-                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="email" 
-                            name="email-address"  
-                            id="email-address"
+class Signin extends Component {
+    render() {
+        return (
+            <div className="relative z13 br3 ba pa3 dark-gray b--black-10 mv4 w-100 w-50-m mw6 shadow-5 w-25-l mw-5 flex justify-center items-center center">
+                <div className="measure tc">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f3 fw6 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                            <label 
+                                className="db fw6 lh-copy f6"
+                                htmlFor="email-address"
+                            >Email</label>
+                            <input 
+                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="email" 
+                                name="email-address"  
+                                id="email-address"
+                            />
+                        </div>
+                        <div className="mv3">
+                            <label 
+                                className="db fw6 lh-copy f6"
+                                htmlFor="password"
+                            >Password</label>
+                            <input 
+                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="password" 
+                                name="password"  
+                                id="password"
+                            />
+                        </div>
+                    </fieldset>
+                    <div className="relative z20">
+                        <input
+                            onClick={() => onRouteChange('home')}
+                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            type="submit" 
+                            value="Sign in"
                         />
                     </div>
-                    <div className="mv3">
-                        <label 
-                            className="db fw6 lh-copy f6"
-                            htmlFor="password"
-                        >Password</label>
-                        <input 
-                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                            type="password" 
-                            name="password"  
-                            id="password"
-                        />
+                    <div className="lh-copy mt3">
+                        <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
                     </div>
-                </fieldset>
-                <div className="relative z20">
-                    <input
-                        onClick={() => onRouteChange('home')}
-                        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                        type="submit" 
-                        value="Sign in"
-                    />
-                </div>
-                <div className="lh-copy mt3">
-                    <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Signin;
